a12036647b082dbf5fd30de76c19a95d69ff0c31
finn_jf <finnfassnacht@gmail.com>
2023-08-03 13:19:26 +0200

    added coords and numbers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const nominatim = require('nominatim-client');
 const express = require("express");
 const serverless = require("serverless-http");
+const data = require("./em-num.json")
 const app = express();
 app.use(express.static("www"));
 app.use(express.json());
 const router = express.Router();
+function em_num(usercountry){
+    let i = 0
+    while (i < data.length) {
+        let country = (data[i]["code"]);
+        if (country == usercountry){
+            let police = (data[i]["police"]);
+            let ambulance = (data[i]["ambulance"]);
+            let fire = (data[i]["fire"]);
+            return([ambulance,fire,police])
+        }
+        i++;
+    }
+}
 router.post("/cords", (req,res) => {
     const client = nominatim.createClient({
         useragent: "Heartbeat",             // The name of your application
@@ -18,8 +32,31 @@ router.post("/cords", (req,res) => {
         };
         console.log(query)
         result = await client.reverse(query)
-        res.send(result)
-        console.log(result)
+        let country = result.address.country
+        let country_code = result.address.country_code
+        let house_num = result.address.house_number
+        let street = result.address.road
+        let town = result.address.town
+        if (town == undefined){
+            town = result.address.city
+        }
+        let suburb = result.address.suburb
+        let emnum = em_num(country_code)
+        let data = {
+            "num":house_num,
+            "street":street,
+            "town":town,
+            "suburb":suburb,
+            "country":country,
+            "emnum":emnum
+        }
+        for (let key in data){
+            if (data[key] === undefined){
+                data[key] = "--"
+            }
+        }
+        console.log(data)
+        res.send(data)
     }
     re_addrs()
 });
--- www/index.js
+++ www/index.js
@@ -9,35 +9,52 @@ async function getaddr(fdata){
         let response = await fetch("/.netlify/functions/index/cords", options)
         let data = await response.json()
         console.log(data)
-        let house_num = data.address.house_number
-        let street = data.address.road
-        let town = data.address.town
-        if (town == undefined){
-            town = data.address.city
-        }
-        let suburb = data.address.suburb
+        let house_num = data.num
+        let street = data.street
+        let town = data.town
+        let suburb = data.suburb
+        let em_num = data.emnum
+        console.log(em_num)
         document.getElementById("locating").remove()
         document.getElementById("street").innerText = street + " " + house_num
         document.getElementById("town").innerText = suburb + " " + town
+        document.getElementById("ems").innerText = em_num[0]
+        document.getElementById("fire").innerText = em_num[1]
+        document.getElementById("police").innerText = em_num[2]
+
        
 }
 async function main(){
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition((position) => {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-            const data = {"lat":lat, "lon":lon}
+            const lat = (position.coords.latitude);
+            const lon = (position.coords.longitude);
+            const data = {"lat":lat, "lon":lon} 
             console.log(data)
             var map = L.map('map').setView([lat, lon], 17);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 18,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',}).addTo(map);
                 var circle = L.circle([lat, lon], {
                     color: 'red',
                     fillColor: '#f03',
                     fillOpacity: 0.5,
                     radius: 12
-                }).addTo(map);
+                })
+                circle.addTo(map);
+
+               L.Control.textbox = L.Control.extend({
+                onAdd: function(map) {
+                    
+                var text = L.DomUtil.create('div');
+                text.style = "position: relative; top:26.8px; right:9.7px"
+                text.id = "info_text";
+                text.innerHTML = "<p style='background-color: white; color:blue; opacity: 0.8; font-size:16px'>"+lat+ " "+ lon+ "</p></div>"
+                return text;
+                },
+            });
+            L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
+            L.control.textbox({ position: 'bottomleft' }).addTo(map);
             getaddr(data)
             
             
