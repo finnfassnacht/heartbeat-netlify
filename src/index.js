ca9f9ce595c3fc3164bcb3051977198ecde62794
finn_jf <finnfassnacht@gmail.com>
2023-12-24 23:03:10 +0100

    Added new AED feature :-)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const nominatim = require('nominatim-client');
 const express = require("express");
 const serverless = require("serverless-http");
 const data = require("./em-num.json")
+const file = require("./data.json");
+const newjson = file
 const app = express();
 app.use(express.static("www"));
 app.use(express.json());
@@ -19,6 +21,68 @@ function em_num(usercountry){
         i++;
     }
 }
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+}
+function nearest(mycords){
+    let cords = []
+    let list = []
+    let list2 = []
+    // get cords
+    for (let x = 0; x < newjson.length; x++){
+      let coords_lat = (newjson[x]["cords"])
+      let entry = coords_lat
+      cords.push(entry)
+     }
+    // create a list with all distances
+    for (let i = 0; i < cords.length; i++){
+        let distance = getDistanceFromLatLonInKm(cords[i][0],cords[i][1],mycords[0],mycords[1])
+        list.push([distance, i])
+    }
+    // strip the index number (i)
+    for (let i = 0; i < list.length; i++){
+        let distance = list[i][0]
+        list2.push(distance)
+    }
+    // sort from short to far
+    list2.sort(function(a, b) {
+        return a - b;
+    });
+    // top 3 best options
+    let top3 = []
+    for (let i = 0; i < 3; i++){
+        let arr = list.filter( function( el ) {
+            return !!~el.indexOf(list2[i])
+       });
+       let final = newjson[arr[0][1]]
+       final.distance = list2[i]
+       top3.push(final)
+    }
+    for (let i = 0; i < 3; i++){
+        if (top3[i].location === undefined){
+            top3[i].location = ""
+        }
+        if (top3[i].name === undefined){
+            top3[i].name = ""
+        }
+    }
+    return top3
+
+}
+
 router.post("/cords", (req,res) => {
     const client = nominatim.createClient({
         useragent: "Heartbeat",             // The name of your application
@@ -42,13 +106,15 @@ router.post("/cords", (req,res) => {
         }
         let suburb = result.address.suburb
         let emnum = em_num(country_code)
+        let AEDs = nearest([query.lat, query.lon])
         let data = {
             "num":house_num,
             "street":street,
             "town":town,
             "suburb":suburb,
             "country":country,
-            "emnum":emnum
+            "emnum":emnum,
+            "AEDs":AEDs
         }
         for (let key in data){
             if (data[key] === undefined){
--- www/index.js
+++ www/index.js
@@ -14,53 +14,99 @@ async function getaddr(fdata){
         let town = data.town
         let suburb = data.suburb
         let em_num = data.emnum
-        console.log(em_num)
+        let AEDs = data.AEDs
         document.getElementById("locating").remove()
         document.getElementById("street").innerText = street + " " + house_num
         document.getElementById("town").innerText = suburb + " " + town
         document.getElementById("ems").innerText = em_num[0]
         document.getElementById("fire").innerText = em_num[1]
         document.getElementById("police").innerText = em_num[2]
+        return AEDs
 
        
 }
+const getCoords = async () => {
+    const pos = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+
+    return {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude,
+    };
+};
+
+function aedinfo(aed){
+
+    let text;
+    if (aed.indoor == "yes"){
+        text = "Indoor AED: "
+    }
+    else{
+        text = "AED: "
+    }
+    document.getElementById("aed_distance").innerText = text + Math.round(1000 *aed.distance) + "m"
+    document.getElementById("name").innerText = aed.name
+    document.getElementById("location").innerText = aed.location
+}
+
 async function main(){
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const lat = (position.coords.latitude);
-            const lon = (position.coords.longitude);
-            const data = {"lat":lat, "lon":lon} 
-            console.log(data)
-            var map = L.map('map').setView([lat, lon], 17);
+        let cords = await getCoords() 
+        console.log(cords)
+        
+
+        var map = L.map('map').setView([cords.lat, cords.lon], 17);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 18,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',}).addTo(map);
-                var circle = L.circle([lat, lon], {
+
+                var circle = L.circle([cords.lat, cords.lon], {
                     color: 'red',
                     fillColor: '#f03',
                     fillOpacity: 0.5,
-                    radius: 12
+                    radius: 6
                 })
                 circle.addTo(map);
-
-               L.Control.textbox = L.Control.extend({
-                onAdd: function(map) {
-                    
-                var text = L.DomUtil.create('div');
-                text.style = "position: relative; top:-26.8px; right:-9.7px"
-                text.id = "info_text";
-                text.innerHTML = "<p style='background-color: white; color:blue; opacity: 0.8; font-size:16px'>"+lat+ " "+ lon+ "</p></div>"
-                return text;
-                },
-            });
-            L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
-            L.control.textbox({ position: 'topright' }).addTo(map);
-            getaddr(data)
-            
-            
-        })
-        } else {
-        document.write("<h3>geolocation not available</h3>");
-        }
+                
+                var aed = L.icon({
+                    iconUrl: 'aed.svg',
+                    iconSize:     [30], // size of the icon
+                    iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+                  
+                });
+                let AEDs = await getaddr(cords)
+                for (let i =0; i < AEDs.length; i++){
+                    L.marker(AEDs[i].cords, {icon:aed}).addTo(map).on('click', function() {
+                        aedinfo(AEDs[i])
+                    });
+                }
+                console.log(AEDs[0])
+                let text;
+                if (AEDs[0].indoor == "yes"){
+                    text = "Indoor AED: "
+                }
+                else{
+                    text = "AED: "
+                }
+                if (AEDs[0].location == undefined){
+                    AEDs[0].location = ""
+                }
+                if (AEDs[0].name == undefined){
+                    AEDs[0].name = ""
+                }
+                document.getElementById("aed_distance").innerText = text + Math.round(1000 *AEDs[0].distance) + "m"
+                document.getElementById("name").innerText = AEDs[0].name
+                L.Control.textbox = L.Control.extend({
+                    onAdd: function(map) {
+                    var text = L.DomUtil.create('div');
+                    text.style = "position: relative; top:-26.8px; right:-9.7px"
+                    text.id = "info_text";
+                    text.innerHTML = "<p style='background-color: white; color:blue; opacity: 0.8; font-size:16px'>"+cords.lat+ " "+ cords.lon+ "</p></div>"
+                    return text;
+                    },
+                });
+                L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
+                L.control.textbox({ position: 'topright' }).addTo(map);
 }
+
 main()
